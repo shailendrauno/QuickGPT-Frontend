452d4be414e089ea64918e556d9b68f5f9f61094
Shailendra Kumar <shailendrauno89@gmail.com>
2025-09-03 20:56:50 +0530

    add purchase section

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -61,6 +61,19 @@ const SideBar = () => {
             </div>
 
 
+
+            {/* Credit Purchases Option */}
+
+            <div onClick={() => {navigate('/credits')}} className='flex items-center gap-2 p-3 mt-4 border border-gray-300
+                dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all'>
+                <img src={assets.diamond_icon} className='w-4.5 dark:invert' alt="" />
+                <div className='flex flex-col text-sm'>
+                    <p>Credits : {user?.credits}</p>
+                    <p className='text-xs text-gray-400'>Purchase credits to use quickgpt</p>
+                </div>
+            </div>
+
+
         </div>
     )
 }
