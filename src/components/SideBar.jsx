7c30d47ef5f4e9eab5d481665442f088b9cd36a2
Shailendra Kumar <shailendrauno89@gmail.com>
2025-09-04 07:27:34 +0530

    Toggle butto addded to change theme

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -74,6 +74,31 @@ const SideBar = () => {
             </div>
 
 
+            {/* Darkmode Toggle */}
+
+
+
+            <div className='flex items-center justify-between gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md'>
+  <div className='flex items-center gap-2 text-sm'>
+    <img src={assets.theme_icon} className='w-4 not-dark:invert' alt="" />
+    <p>Dark Mode</p>
+  </div>
+  
+  <label className='relative inline-flex cursor-pointer'>
+    <input
+      onChange={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+      type="checkbox"
+      className="sr-only peer"
+      checked={theme === 'dark'}
+    />
+    <div className='w-9 h-5 bg-gray-400 rounded-full peer-checked:bg-purple-600 transition-all'>
+    </div>
+    <span className='absolute left-1 top-1 w-3 h-3 bg-white rounded-full transition-transform peer-checked:translate-x-4'></span>
+  </label>
+</div>
+
+
+
         </div>
     )
 }
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,58 +1,60 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {dummyChats, dummyUserData} from "../assets/assets"
+import { dummyChats, dummyUserData } from "../assets/assets"
 
 
 
 const AppContext = createContext()
 
 
-export const AppContextProvider = ({children})=>{
+export const AppContextProvider = ({ children }) => {
 
-const navigate = useNavigate()
-const [user, setUser] = useState(null)
-const [chats, setChats] = useState([])
-const [selectedChat, setSelectedChat] = useState(null)
-const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+    const navigate = useNavigate()
+    const [user, setUser] = useState(null)
+    const [chats, setChats] = useState([])
+    const [selectedChat, setSelectedChat] = useState(null)
+    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
 
-const fetchUser = async ()=>{
-    setUser(dummyUserData)
-}
-
-
-const fetchUserChats = async() =>{
-    setChats(dummyChats)
-    setSelectedChat(dummyChats[0])
-}
-
-useEffect(()=>{
-    if(theme === 'dark'){
-        document.documentElement.classList.add('dark')
-    }else{
-        document.documentElement.classList.remove('dark')
+    const fetchUser = async () => {
+        setUser(dummyUserData)
     }
-},[theme])
 
-useEffect(()=>{
-    if(user){
-        fetchUserChats()
-    }else{
-        setChats([])
-        setSelectedChat(null)
+
+    const fetchUserChats = async () => {
+        setChats(dummyChats)
+        setSelectedChat(dummyChats[0])
     }
-},[user])
+
+    useEffect(() => {
+        if (theme === 'dark') {
+            document.documentElement.classList.add('dark')
+        } else {
+            document.documentElement.classList.remove('dark')
+        }
+        localStorage.setItem('theme', theme)
+    }, [theme])
+
+    useEffect(() => {
+        if (user) {
+            fetchUserChats()
+        } else {
+            setChats([])
+            setSelectedChat(null)
+        }
+    }, [user])
 
 
-useEffect(()=>{
-    fetchUser()
-},[])
+    useEffect(() => {
+        fetchUser()
+    }, [])
 
 
 
     const value = {
-        navigate, user, setUser, fetchUser, chats, setChats, selectedChat, setSelectedChat, theme
+        navigate, user, setUser, fetchUser, chats,
+        setChats, selectedChat, setSelectedChat, theme, setTheme,
     }
-    return(
+    return (
         <AppContext.Provider value={value}>
             {children}
         </AppContext.Provider>
