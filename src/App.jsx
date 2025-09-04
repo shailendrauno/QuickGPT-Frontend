b6b37e3a6751b48a1b751a064da6bfe0c99d26de
Shailendra Kumar <shailendrauno89@gmail.com>
2025-09-04 18:41:49 +0530

    Front end completed

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,12 @@ import Community from './pages/Community'
 import { assets } from './assets/assets'
 import './assets/prism.css'
 import Loading from './pages/Loading'
+import { useAppContext } from './context/AppContext'
+import Login from './pages/Login'
 
 const App = () => {
+
+  const {user} = useAppContext()
 
   const [isMenuOpen, setIsMenuOpen] = useState(false)
   const {pathname} = useLocation()
@@ -23,7 +27,8 @@ const App = () => {
       onClick={()=>setIsMenuOpen(true)}/>}
 
 
-    <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white'>
+      {user? (
+        <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white'>
       <div className='flex h-screen w-screen'>
         <SideBar  isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen}/>
         <Routes>
@@ -33,6 +38,13 @@ const App = () => {
         </Routes>
       </div>
     </div>
+      ) : (
+        <div className='bg-gradient-to-b from-[#242124] to-[#000000] flex items-center justify-center h-screen w-screen'>
+          <Login />
+        </div>
+      )}
+
+    
     </>
   )
 }
